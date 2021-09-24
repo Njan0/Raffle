7757d1f7fc91f1e3d1f7c1158919631212da4c0b
Njan0 <Jan.T.H.Nguyen@gmail.com>
2021-09-24 12:14:37 +0200

    hash passwords, use jwt, use dotenv

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const mysql = require('mysql');
 const express = require('express');
-const session = require('express-session');
+const cookieParser = require('cookie-parser')
 const requestIp = require('request-ip');
 const { v4: uuidv4, validate: validateUUID } = require('uuid');
+require('dotenv').config()
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const OPEN = 'Open';
 const CLOSED = 'Closed';
 const port = 8081;
 
 const con = mysql.createConnection({
-	host     : 'localhost',
-	port 	 : '3306',
-	user     : 'root',
-	password : 'pw',
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
 	database : 'raffle'
 });
 
@@ -20,11 +23,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(requestIp.mw());
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
+app.use(cookieParser());
 app.use(express.urlencoded({extended : false}));
 
 app.get('/', function(req, res) {
@@ -37,26 +36,30 @@ function raffleURL(id) {
 }
 
 // create a new raffle
-app.post('/postraffle', function(req, res) {
-	var name = req.body.name;
-	var password = req.body.password;
+app.post('/postraffle', async function(req, res) {
+	const { name, password } = req.body;
 	if (name && password) {
 		if (name.length <= 255 && password.length <= 255) {
 			// insert to db
 			id = uuidv4();
-			var sql = "INSERT INTO raffles(id, name, password) VALUES(UUID_TO_BIN(?),?,?)";
-			con.query(sql, [id, name, password], function (err, result) {
-				if (err) throw err;
+			bcrypt.hash(password, 10, function(err, hash) {
+				const sql = "INSERT INTO raffles(id, name, password) VALUES(UUID_TO_BIN(?),?,?)";
+				con.query(sql, [id, name, hash], function (err, result) {
+					if (err) throw err;
+				});
+
+				// create token
+				const token =  jwt.sign(
+					{ id: id },
+					process.env.TOKEN_KEY
+				);
+
+				// store as cookie
+				res.cookie(id, token, { httpOnly: true });
+
+				// redirect to newly created raffle
+				res.redirect(raffleURL(id));
 			});
-
-			// store raffle for this session
-			if (!req.session.raffles) {
-				req.session.raffles = [];
-			}
-			req.session.raffles.push(id);
-
-			// redirect to newly created raffle
-			res.redirect(raffleURL(id));
 		} else {
 			res.send('Name or password too long!');
 		}
@@ -68,13 +71,13 @@ app.post('/postraffle', function(req, res) {
 // extract raffle and apply function
 function applyRaffle(id, fun) {
 	// get raffle informations
-	var sql = "SELECT * FROM raffles WHERE id = UUID_TO_BIN(?)";
+	const sql = "SELECT * FROM raffles WHERE id = UUID_TO_BIN(?)";
 	con.query(sql, [id], function (err, result) {
 		if (err) throw err;
 	
-		var raffleName = id;
-		var status = null;
-		var raffleResult = null;
+		let raffleName = id;
+		let status = null;
+		let raffleResult = null;
 		// extract info from result
 		if (result.length > 0) {
 			raffleName = result[0].name;
@@ -86,9 +89,24 @@ function applyRaffle(id, fun) {
 	});
 }
 
+function checkToken(id, req) {
+	// check if token exists
+	if (!req.cookies[id]) {
+		return false;
+	}
+
+	try {
+		// verify token
+		return jwt.verify(req.cookies[id], process.env.TOKEN_KEY).id == id
+	} catch (err) {
+		// invalid token
+		return false;
+	}
+}
+
 // inspect a raffle
 app.get('/raffle/:id/', function(req, res) {
-	var id = req.params.id;
+	const id = req.params.id;
 	if (!validateUUID(id)) {
 		res.sendStatus(404);
 		return;
@@ -97,8 +115,7 @@ app.get('/raffle/:id/', function(req, res) {
 	applyRaffle(id, function (raffle) {
 		if (raffle.status) {
 			// check if pw needed
-			var rsf = req.session.raffles;
-			var canClose = rsf && rsf.includes(id);
+			const canClose = checkToken(id, req);
 			
 			res.render('raffle', {
 				raffleName: raffle.name,
@@ -114,22 +131,22 @@ app.get('/raffle/:id/', function(req, res) {
 
 // join raffle interface
 app.get('/raffle/:id/join', function(req, res) {
-	var id = req.params.id;
+	const id = req.params.id;
 	if (!validateUUID(id)) {
 		res.sendStatus(404);
 		return;
 	}
 
-	var owner = req.clientIp;
+	const owner = req.clientIp;
 	applyRaffle(id, function (raffle) {
 		status = raffle.status;
 		if (status === OPEN) {
 			// check if user already added a ticket
-			var sql = "SELECT * FROM tickets WHERE raffleID = UUID_TO_BIN(?) AND owner = ?";
+			const sql = "SELECT * FROM tickets WHERE raffleID = UUID_TO_BIN(?) AND owner = ?";
 			con.query(sql, [id, owner], function (err, result) {
 				if (err) throw err;
 
-				var action = result.length > 0 ? 'Update' : 'Add';
+				const action = result.length > 0 ? 'Update' : 'Add';
 				res.render('join', {
 					raffleName: raffle.name,
 					action : action
@@ -147,13 +164,13 @@ app.get('/raffle/:id/join', function(req, res) {
 
 // add ticket to raffle
 app.post('/raffle/:id/apply', function(req, res) {
-	var id = req.params.id;
+	const id = req.params.id;
 	if (!validateUUID(id)) {
 		res.sendStatus(404);
 		return;
 	}
-	var owner = req.clientIp;
-	var content = req.body.content;
+	const owner = req.clientIp;
+	const content = req.body.content;
 
 	if (content.length > 255) {
 		res.send('Text of ticket too long!');
@@ -164,8 +181,8 @@ app.post('/raffle/:id/apply', function(req, res) {
 		// if raffle open
 		if (raffle.status === OPEN) {
 			// add ticket to raffle. update if user already added a raffle.
-			var sql = `INSERT INTO tickets (raffleID, owner, content) VALUES (UUID_TO_BIN(?),?,?)
-					   ON DUPLICATE KEY UPDATE content=VALUES(content)`
+			const sql = `INSERT INTO tickets (raffleID, owner, content) VALUES (UUID_TO_BIN(?),?,?)
+					     ON DUPLICATE KEY UPDATE content=VALUES(content)`
 			con.query(sql, [id, owner, content], function (err, result) {
 				if (err) throw err;
 			});
@@ -182,17 +199,17 @@ function closeRaffle(id) {
 		// if raffle is open
 		if (raffle.status == OPEN) {
 			// get random ticket in raffle
-			var randomSql = "SELECT * FROM tickets WHERE raffleID = UUID_TO_BIN(?) ORDER BY RAND() LIMIT 1";
+			const randomSql = "SELECT * FROM tickets WHERE raffleID = UUID_TO_BIN(?) ORDER BY RAND() LIMIT 1";
 			con.query(randomSql, [id], function (err, result) {
 				if (err) throw err;
 
-				var content = '';
+				let content = '';
 				if (result.length > 0) {
 					content = result[0].content;
 				}
 
 				// set raffle to closed and result to content of selected raffle
-				var sql = "UPDATE raffles SET status=?,result=? WHERE id=UUID_TO_BIN(?)";
+				const sql = "UPDATE raffles SET status=?,result=? WHERE id=UUID_TO_BIN(?)";
 				con.query(sql, [CLOSED, content, id], function (err, result) {
 					if (err) throw err;
 				});
@@ -203,29 +220,37 @@ function closeRaffle(id) {
 
 // close raffle request
 app.post('/raffle/:id/close', function(req, res) {
-	var id = req.params.id;
+	const id = req.params.id;
 	if (!validateUUID(id)) {
 		res.sendStatus(404);
 		return;
 	}
 	
-	var rsf = req.session.raffles
-	if (!(rsf && rsf.includes(id))) {
-		// raffle not created in this session. need pw.
-		var pw = req.body.password;
+	if (!checkToken(id, req)) {
+		// no token. need pw.
+		const pw = req.body.password;
+		
 		// check if correct pw given
-		var sql = "SELECT * FROM raffles WHERE id = UUID_TO_BIN(?) AND password = ?";
-		con.query(sql, [id, pw], function (err, result) {
+		const sql = "SELECT * FROM raffles WHERE id = UUID_TO_BIN(?)";
+		con.query(sql, [id], function (err, result) {
 			if (result.length > 0) {
-				// pw match
-				closeRaffle(id);
-				res.redirect('back');
+				bcrypt.compare(pw, result[0].password, function(err, result) {
+					if (result) {
+						// pw match
+						closeRaffle(id);
+						res.redirect('back');
+					} else {
+						// wrong pw
+						res.send('Invalid password!');
+					}
+				});
 			} else {
-				res.send('Invalid password!');
+				// raffle does not exist
+				res.sendStatus(404);
 			}
 		});
 	} else {
-		// raffle created in this session. can close without pw.
+		// has token. can close without pw.
 		closeRaffle(id);
 		res.redirect('back');
 	}
